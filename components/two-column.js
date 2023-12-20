d2fe1774e529c3ea43e92876e8e16e496dd97df1
itc-s23004 <s23004@std.it-college.ac.jp>
2023-12-20 07:03:45 +0000

    4.8 p128~p134

---
+++ components/two-column.js
@@ -0,0 +1,15 @@
+import styles from 'styles/two-column.module.css'
+
+export const TwoColumn = ({ children }) => {
+  return <div className={styles.flexContainer}>{children}</div>
+}
+
+export default TwoColumn
+
+export const TwoColumnMain = ({ children }) => {
+  return <div className={styles.main}>{children}</div>
+}
+
+export const TwoColumnSidebar = ({ children }) => {
+  return <div className={styles.sidebar}>{children}</div>
+}
