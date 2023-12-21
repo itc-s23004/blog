d9518cd9e22457ab1102837271fac464ba6ab0e7
itc-s23004 <s23004@std.it-college.ac.jp>
2023-12-21 07:52:09 +0000

    5.7 end

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '@/styles/globals.css'
 import Layout from 'components/layout'
+
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.sutoAddCss = false
+
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
