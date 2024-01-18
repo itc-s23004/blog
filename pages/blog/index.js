714984ae53304a749e43e5cc2aec20f76150bdc5
itc-s23004 <s23004@std.it-college.ac.jp>
2024-01-18 07:32:16 +0000

    9.6 end

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,9 @@ import Container from 'components/container'
 import Hero from 'components/hero'
 import { getAllPosts } from 'lib/api'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
+
 const Blog = ({ posts }) => {
   return (
     <Container>
@@ -16,6 +19,14 @@ export default Blog
 
 const getStaticProps = async () => {
   const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
 
   return {
     props: {
